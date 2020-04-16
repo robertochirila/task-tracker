d1d3a56d823ac7c511fac60d269301baefa830a2
Roberto Chirila <robertochirila@yahoo.com>
2020-04-16 12:46:22 +0100

    first commit

---
+++ src/components/TaskForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TaskForm extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default TaskForm
